86c2695485ee1c0e7fe32deacbf6109b5638b1ea
Kyle <kylejbrown17@gmail.com>
2017-06-03 16:32:18 +0000

    Added bash script to run the network with appropriate hyperparameters on google cloud

---
+++ network_manaager.sh
@@ -0,0 +1,4 @@
+python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-3 --num_epochs1=1
+python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-4 --num_epochs1=1
+python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-5 --num_epochs1=1
+python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-6 --num_epochs1=1
