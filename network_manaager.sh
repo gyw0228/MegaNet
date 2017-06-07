bac7349ffe6bbdcd216d870b7e4db6be422161e4
Kyle <kylejbrown17@gmail.com>
2017-06-07 01:27:12 +0000

    network manager got overwritten last time :(

--- network_manaager.sh
+++ network_manaager.sh
@@ -1,4 +1,1 @@
-python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-3 --num_epochs1=1
-python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-4 --num_epochs1=1
-python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-5 --num_epochs1=1
-python ~/MegaNet/src/ResKPNet.py --base_dir=/home/kyle/Project/coco --model_path=/home/kyle/MegaNet/checkpoints/resnet_v2_50.ckpt --learning_rate1=1e-6 --num_epochs1=1
+python ~/MegaNet/src/HourGlassNet.py --base_dir=/home/kyle/Project/coco --learning_rate1=5e-3 --num_epochs1=10 --batch_size=10 --summary_every=200 --decay_steps=5000 --summary_every=200 --log_path=/home/kyle/Hourglass/logs
